1eb1f55d8ef105441113372a844505c71ec7000e
Ramin <rsedighi@gmail.com>
2014-03-17 20:17:46 -0700

    added constants

---
+++ constants.rb
@@ -0,0 +1,30 @@
+#Constants
+#
+#Not used in Ruby all that often
+#Used a lot like variables.
+# => not true objects
+# => point to objects.
+#Constant is constant. It stays the same. Variables change overtime!
+
+test = 1
+# 1
+TEST = 2
+# 2
+test
+# 1
+TEST
+# 2
+
+#anything that begins with a capital letter is a constant!
+
+Hello = 10
+# 10
+
+test = 100
+# 100
+
+TEST = 100
+#warning, already initialized constant TEST
+TEST
+# 100
+
